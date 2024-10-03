8604971ef166efb90790a7c2a0bfd95dedd99974
Tumanov-Nikita <grooov173@yandex.ru>
2024-10-03 16:47:54 +0300

    added timer and farmButton

--- src/app.js
+++ src/app.js
@@ -1,51 +1,33 @@
 const circle = document.querySelector('#circle');
 const score = document.querySelector('#score');
+const farmButton = document.querySelector('#farmButton');
 
 window.onload = function() {
     setScore(getScore())
+    initTimer();
 }
 
 function setScore(scoreValue) {
+    if (scoreValue < 0){
+        return;
+    }
     localStorage.setItem('score', scoreValue);
-    score.textContent = scoreValue;
+    score.textContent = new Intl.NumberFormat("ru", {style: "decimal", minimumFractionDigits: 2}).format(scoreValue) + " $OZON";
 }
 
 function getScore() {
-    return Number(localStorage.getItem('score')) ?? 0
+    return Number(localStorage.getItem('score')) ?? 1000
 }
 
-function addOne(){
-    setScore(getScore() + 1);
+function initTimer() {
+    timerAction();
 }
 
+function timerAction() {
+    setScore(getScore() - 0.01)
+    setTimeout(timerAction, 1000);
+}
 
-circle.addEventListener('click', (event) => {
-    const rect = circle.getBoundingClientRect();
-    const offsetX = event.clientX - rect.left - rect.width / 2;
-    const offsetY = event.clientY - rect.top - rect.height / 2;
-
-    const DEG = 40;
-
-    const tiltX = (offsetY / rect.height) * DEG;
-    const tiltY = (offsetX / rect.width) * -DEG;
-
-    circle.style.setProperty('--tiltX', `${tiltX}deg`);
-    circle.style.setProperty('--tiltY', `${tiltY}deg`);
-
-    setTimeout(() => {
-    circle.style.setProperty('--tiltX', `0deg`);
-    circle.style.setProperty('--tiltY', `0deg`);
-    }, 300);
+farmButton.addEventListener('click', (event) => {
     
-    const plusOne = document.createElement('div');
-    plusOne.classList.add('plus-one');
-    plusOne.textContent = '+1';
-    plusOne.style.left = `${event.clientX - rect.left}px`;
-    plusOne.style.top = `${event.clientY - rect.top}px`;
-    addOne()
-    circle.parentElement.appendChild(plusOne);
-    setTimeout(() => {
-        plusOne.remove()
-    }, 500)
-
 })
