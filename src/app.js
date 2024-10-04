a95b85fb41c3cb3c69db07781852ea55b2770d23
ntumanov <grooov173@yandex.ru>
2024-10-05 01:44:41 +0400

    trying to implement progress bar

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const circle = document.querySelector('#circle');
 const score = document.querySelector('#score');
 const farmButton = document.querySelector('#farmButton');
+const canvas = document.querySelector("#canvasButton");
+var i = 0;
 
 window.onload = function() {
     setScore(getScore())
@@ -15,6 +17,11 @@ function setScore(scoreValue) {
     score.textContent = new Intl.NumberFormat("ru", {style: "decimal", minimumFractionDigits: 2}).format(scoreValue) + " $OZON";
 }
 
+farmButton.addEventListener('click', (event) => {
+    console.log("Кнопка нажата");
+    move();
+})
+
 function getScore() {
     return Number(localStorage.getItem('score')) ?? 1000
 }
@@ -24,10 +31,36 @@ function initTimer() {
 }
 
 function timerAction() {
-    setScore(getScore() - 0.01)
+    setScore(getScore() - 0.01);
     setTimeout(timerAction, 1000);
 }
 
-farmButton.addEventListener('click', (event) => {
-    
-})
+
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("farmButton");
+        var width = 1;
+        var id = setInterval(frame, 10);      
+        
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+            i = 0; //#00DDEB
+            } else {
+                width++;
+                var ctx = canvas.getContext("2d");
+                var gradient = ctx.createLinearGradient(0, 0, width, 0);
+                // Добавление трёх контрольных точек
+                gradient.addColorStop(0, "green");
+                gradient.addColorStop(0.5, "cyan");
+                gradient.addColorStop(1, "green");
+
+                // Установка стиля заливки и отрисовка прямоугольника градиента
+                ctx.fillStyle = gradient;
+                ctx.fillRect(20, 20, 200, 100);
+            }
+        }
+    }
+}
+
