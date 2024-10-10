81c373eafff2aefde6d1323adab297a2c94caff3
Tumanov-Nikita <grooov173@yandex.ru>
2024-10-11 02:11:19 +0300

    mvp fixes

--- src/app.js
+++ src/app.js
@@ -3,15 +3,18 @@ const score = document.querySelector('#score');
 const farmButton = document.querySelector('#farmButton');
 const canvas = document.querySelector("#canvasButton");
 const myBar = document.querySelector("#myBar");
-var i = 0;
+const modif = 360;
 
 window.onload = function() {
     setScore(getScore())
+    localStorage.setItem('dueDate', getDueDate());
+    localStorage.setItem('farmedScore', getScore());
     initTimer();
     if (Number(localStorage.getItem('direction')) > 1)
     {
-        processingProgressBar();
+        processingProgressBarWidth();
     }
+    processingProgressBarText();
 }
 
 function initTimer() {
@@ -23,42 +26,47 @@ function timerAction() {
     var currentDate = new Date().getTime();
     var dueDate = getDueDate();
     var direction = Number(localStorage.getItem('direction'));
-
     if (direction <= 0)
     {
+        var farmedScore = localStorage.getItem('farmedScore');
+        var lostScore = Number((currentDate / 1000) - (dueDate / 1000)) / 100;
+        setScore(Number(farmedScore) - Number(lostScore));
+        
         localStorage.setItem('direction', -1);
-        setScore(getScore() - 0.01);
         setTimeout(timerAction, 1000);
     }
     else
     {
+        var farmValue = Number((currentDate / 1000) - (startDate / 1000)) / 10;
         if (dueDate - currentDate >= 0)
         {
-            var farmValue = Number((currentDate / 1000) - (startDate / 1000)) / 10;
-            if (startDate==currentDate==dueDate)
+            if (startDate==currentDate && currentDate==dueDate)
             {
                 localStorage.setItem('startDate', startDate);
             }
-            if (farmValue > 2160)
+            if (farmValue > 6 * modif)
             {
-                farmValue = 2160;
+                farmValue = 6 * modif;
             }
             localStorage.setItem('farmValue', farmValue);
+            processingProgressBarText();
             setTimeout(timerAction, 1000);
         }
-        else if (farmValue == 2160)
+        else if (farmValue >= 6 * modif)
         {
             localStorage.setItem('direction', -1);
-            myBar.textContent = "Забрать 2160 $OZON на баланс";
+            localStorage.setItem('farmValue', farmValue);
+            localStorage.setItem('farmedScore', getScore());
+            myBar.textContent = `Забрать ${6 * modif} $OZON на баланс`;
             setTimeout(timerAction, 1000);
         }
         else
         {
             setTimeout(timerAction, 1000);
+            processingProgressBarText();
         }
     }
-
-    processingProgressBar();
+    processingProgressBarWidth();
 }
 
 farmButton.addEventListener('click', (event) => {
@@ -66,17 +74,17 @@ farmButton.addEventListener('click', (event) => {
     var direction = Number(localStorage.getItem('direction'));
     if (direction < 0)
     {
-        var farmedValue = localStorage.getItem('farmValue') ?? 0;
-        setScore(getScore() + Number.parseInt(farmedValue));
-        localStorage.setItem('initialScore', getScore());
+        var farmValue = localStorage.getItem('farmValue') ?? 0;
+        setScore(getScore() + Number.parseInt(farmValue));
         setDeltaScore(1);
-        processingProgressBar();
+        processingProgressBarText();
+        processingProgressBarWidth();
     }
 })
 
 function addMinutesToDate(date, minutes) {
     const minutesToAdd = minutes * 60 * 1000;
-    date.setTime(date.getTime() + minutesToAdd);
+    date += minutesToAdd;
     return date;
 }
 
@@ -84,8 +92,8 @@ function setDeltaScore(direction) {
     if (direction > 0)
     {
         var currentDate = new Date();
-        localStorage.setItem('startDate', currentDate)
-        var dueDate = addMinutesToDate(currentDate, 360);
+        localStorage.setItem('startDate', currentDate.getTime())
+        var dueDate = addMinutesToDate(currentDate.getTime(), modif);
         localStorage.setItem('dueDate', dueDate);
         localStorage.setItem('direction', direction);
     }
@@ -111,51 +119,71 @@ function getScore() {
 }
 
 function getStartDate() {
-    var startDate = localStorage.getItem('startDate');
-    if (!startDate || startDate == NaN)
+    var startDate = Number(localStorage.getItem('startDate'));
+    if (isNaN(startDate) || startDate == 0)
     {
         return new Date().getTime();
     }
     else 
     {
-        return Date.parse(startDate);
+        return startDate;
     }
 }
 
 function getDueDate() {
-    var dueDate = localStorage.getItem('dueDate');
-    if (!dueDate || dueDate == NaN)
+    var dueDate = Number(localStorage.getItem('dueDate'));
+    if (isNaN(dueDate) || dueDate == 0)
     {
         return new Date().getTime();
     }
     else 
     {
-        return Date.parse(dueDate);
+        return dueDate;
     }
 }
 
-function processingProgressBar() {
-    var remainingTime = dateWithoutTimezone(new Date(getDueDate() - new Date().getTime()));
+function getRemainingTimeString() {
+    var remainingTime = new Date(dateWithoutTimezone(getDueDate() - new Date().getTime()));
     var hours = remainingTime.getHours();
     var minutes = remainingTime.getMinutes();
 
-    var farmValue = localStorage.getItem('farmValue');
-    
-    width = (Number.parseInt(farmValue)/2160)*100
-    if (farmValue > 0)
+    return `${hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0')}`
+}
+
+function processingProgressBarText() {
+    var farmValue = Number(localStorage.getItem('farmValue'));
+    if (farmValue >= 6*modif)
     {
-        myBar.style.width = width + "%";
+        myBar.textContent = `Забрать ${6 * modif} $OZON на баланс`;
+    }
+    else if (Number(localStorage.getItem('direction')) > 0)
+    {
         myBar.textContent = `Фарминг ${
             new Intl.NumberFormat("ru", {
                 style: "decimal", minimumFractionDigits: 1, maximumFractionDigits: 1
             }).format(farmValue)
-        } / 2160\t${hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0')}`;
+        } / ${6*modif}\t${getRemainingTimeString()}`;
+    }
+    else
+    {
+        myBar.textContent = "Начать фарминг озонового слоя";
+    }
+}
+
+function processingProgressBarWidth() {
+    var farmValue = localStorage.getItem('farmValue');
+    
+    width = (Number.parseInt(farmValue)/(6*modif))*100
+    if (farmValue > 0)
+    {
+        myBar.style.width = width + "%";
     }
 }
 
 function dateWithoutTimezone(date) {
-    const tzoffset = date.getTimezoneOffset() * 60000;
-    const withoutTimezone = new Date(date.valueOf() + tzoffset);
+    var dateWithTimezone = new Date(date);
+    const tzoffset = dateWithTimezone.getTimezoneOffset() * 60000;
+    const withoutTimezone = dateWithTimezone.valueOf() + tzoffset;
     return withoutTimezone;
 }
 
